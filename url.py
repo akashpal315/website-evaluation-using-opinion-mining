b1cab6f6601675cf65327f15208d101a2d0c24d6
Akash Pal <akashpal315@users.noreply.github.com>
2017-02-15 15:57:00 +0530

    Extracting information from websites.

---
+++ url.py
@@ -0,0 +1,10 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+wiki = "https://en.wikipedia.org/wiki/List_of_state_and_union_territory_capitals_in_India"
+
+page = urllib.request.urlopen(wiki)
+
+soup = BeautifulSoup(page)
+
+print(soup.prettify())
